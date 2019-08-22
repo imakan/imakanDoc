14002da1009a01714b0ff26e64c3acb601a02ea8
makan <makan@staff.sina.com.cn>
2019-08-23 00:29:17 +0800

    update

--- 练习题/binarySearch.js
+++ 练习题/binarySearch.js
@@ -1,30 +1,24 @@
 /**
  * 二分查找法
- * 首先明确一点就是二分查找法是一个有序数组
- * 为什么要是一个有序数组呢？因为我们在实现的时候需要判断中间值，和目标朱的大小，大于的话，查找初始点需要往后移动。
- * 基本思想：
- * 初始化一个数组的长度-1，定义为max 因为arr[max + min ] 需要-1
- * 初始化一个0,定义为min
- * 获取这个最大值和min之间的中间长度
- * 目标值大于数组中间值，说明目标值在数组中间值右边，此时我们再取中间值，应该是原来的数组长度的中间数+1 到数组长度
- * 目标值小于数组中间值。说明目标值在数组中间值的左边，此时我们再取中间值，应该是原来的数组长度的中间值到0
- *
+ * 先明确一点：二分查找法所查找的数组必须是个有序数组
+ * 为什么要是有序数组，因为我们在查找目标元素的时候，需要判断目标值和中间值的大小，如果大于这个中间值，那么我们往后面查找，如果不是有序的，就不能判断了
+ * 基本原理：
+ * 目标值和我们定义的中间值对比，如果大于中间值，则中间值往后移动，如果小于中间值，则中间值往前移动
  */
-let arr = [1, 2, 3, 4, 5, 6, 7]; //
-let binarySearch = (arr, dest) => {
-  let max = arr.length-1; // 获取数组长度
-  let min = 0; // 定义 m 到 length的长度区间
+let arr = [1, 2, 3, 4, 5, 6, 7];
+let inserSort = (arr, dest) => {
+  let max = arr.length - 1;
+  let min = 0;
   while (min <= max) {
-    let mid = Math.floor((min + max) / 2);
+    let mid = Math.floor((max + min) / 2);
+    if (arr[mid] === dest) return mid;
     if (dest > arr[mid]) {
       min = mid + 1;
-    } else if (dest < arr[mid]) {
-      max = mid - 1;
     } else {
-      return mid;
+      max = mid - 1;
     }
   }
   return -1;
 };
 
-console.log(binarySearch(arr, 8));
+console.log(inserSort(arr, 1));
